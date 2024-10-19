0e38f82fd6b9ee866af4e5d80846691e673ac152
Abope-1212 <waleagbeniga06@gmail.com>
2024-10-19 02:09:48 +0100

    night

--- app/meals/share/page.js
+++ app/meals/share/page.js
@@ -1,9 +1,13 @@
+"use client";
+import { useActionState } from "react";
 import ImagePicker from "@/componets/meals/image-picker";
 import classes from "./page.module.css";
 import { shareMeal } from "@/lib/action";
+import MealsFormSubmit from "@/componets/meals/meals-form-submit";
+
 
 export default function ShareMealPage() {
-
+  const [state, formAction] = useActionState(shareMeal, { message: null });
 
   return (
     <>
@@ -14,7 +18,7 @@ export default function ShareMealPage() {
         <p>Or any other meal you feel needs sharing!</p>
       </header>
       <main className={classes.main}>
-        <form className={classes.form} action={shareMeal}>
+        <form className={classes.form} action={formAction}>
           <div className={classes.row}>
             <p>
               <label htmlFor="name">Your name</label>
@@ -43,8 +47,9 @@ export default function ShareMealPage() {
             ></textarea>
           </p>
           <ImagePicker lable="Your image" name="image" />
+          {state.message && <p>{state.message}</p>}
           <p className={classes.actions}>
-            <button type="submit">Share Meal</button>
+            <MealsFormSubmit />
           </p>
         </form>
       </main>
--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
+// @type {import('next').NextConfig}
+const nextConfig = {
+  webpack(config) {
+    // Remove the alias for 'url' to prevent the overwrite of the node 'url' module
+    delete config.resolve.alias.url;
 
-module.exports = nextConfig
+    return config;
+  },
+};
+
+module.exports = nextConfig;
